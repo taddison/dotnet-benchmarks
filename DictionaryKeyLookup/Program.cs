59cdf4aeedc1af7b6eac6d6e67f0779ffb061cce
Timothy Addison <git@tjaddison.com>
2022-02-20 11:11:45 -0500

    3 benchmarks per-runtime

--- DictionaryKeyLookup/Program.cs
+++ DictionaryKeyLookup/Program.cs
@@ -12,7 +12,8 @@ public class DictionaryBenchmark
 {
   private static readonly Dictionary<string, int> _dictionary = new Dictionary<string, int>();
   private const string KEY_STRING = "abcdefghijklmnopqrstuvwxyz";
-  private const int TARGET_MAX_LENGTH = 200;
+  private const int MAX_KEY_LENGTH = 200;
+  private const int KEY_LENGTH_INCREMENT = 100;
 
   [GlobalSetup]
   public void Setup()
@@ -27,16 +28,16 @@ public class DictionaryBenchmark
   [ArgumentsSource(nameof(GetKeyValues))]
   public int TryGetValue(string key) => _dictionary.TryGetValue(key, out var value) ? value : default;
 
-//https://benchmarkdotnet.org/articles/features/parameterization.html#sample-introargumentssource
+  //https://benchmarkdotnet.org/articles/features/parameterization.html#sample-introargumentssource
   public IEnumerable<object> GetKeyValues()
   {
     var keyString = KEY_STRING;
-    while(keyString.Length < TARGET_MAX_LENGTH)
+    while(keyString.Length < MAX_KEY_LENGTH)
     {
       keyString += KEY_STRING;
     }
 
-    for (int i = 1; i <= keyString.Length; i += 20)
+    for (int i = 1; i <= keyString.Length; i += KEY_LENGTH_INCREMENT)
     {
       yield return keyString.Substring(0, i);
     }
